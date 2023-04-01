d9cd80ab43719e3a51cb5999bf5c65c9c8e8e8d0
SivaPrasad43 <39110552+SivaPrasad43@users.noreply.github.com>
2023-04-01 20:50:42 +0530

    setupBox :selectBox-Toggling

--- src/components/SetupBox/Setup.tsx
+++ src/components/SetupBox/Setup.tsx
@@ -2,47 +2,50 @@ import React from 'react'
 import './Setup.css'
 import setupImg from '../../assets/Kindergarten student-bro 1.png'
 
-const Setup = ({activeItem}: {activeItem: string})=> {
-  return (
-      <div className="white-container">
-                <h3>Setup a new {activeItem}</h3>
-                <div className="setup-club">
-                    <div className="setup-filter">
-                        <div className="select-container club">
-                            <div className="setup-item" id="district">
-                                <p>District</p>
-                                <select id="district_select">
-                                    <option value="{{district.id}}">Select District</option>
-                                </select>
-                            </div>
-                            <div className="setup-item hidden" id="panchayath">
-                                <p>Panchayath</p>
-                                <select id="panchayath_select">
-                                    <option >Select Panchayath</option>
-                                </select>
-                            </div>
+const schoolItems = ['D1','D2','D3','D4']
+const club = ['C1','C2','C3','C4','C5']
 
-                            <div className="setup-item" id="college">
-                                <p id="institute">College</p>
-                                <select name="" id="college_school_select">
-                                    <option value="">Select College</option>
-                                </select>
-                            </div>
-                            <div className="setup-item">
-                                <p>Status</p>
-                                <select name="" id="status_select">
-                                    <option value="">Select Status</option>
-                                </select>
-                            </div>
-                            <button id="create_btn" className="black-btn" >Create</button>       
-                        </div>
+interface SelectItemProps{
+    item: string 
+}
+
+const SelectItem: React.FC<SelectItemProps> = ({ item }) => {
+    return (
+        <div className="setup-item" id="district">
+            <p>{item}</p>
+            <select id="district_select">
+                <option value="{{district.id}}">Select Value</option>
+            </select>
+        </div>
+    );
+};
+
+const Setup = ({ activeItem }: { activeItem: string }) => {
+    let itemsToRender: string[] = [];
+    console.log("this is ",activeItem)
+    if (activeItem === "Model School") {
+        itemsToRender = schoolItems
+    }else{
+        itemsToRender = club
+    }
+    return (
+        <div className="white-container">
+            <h3>Setup a new {activeItem}</h3>
+            <div className="setup-club">
+                <div className="setup-filter">
+                    <div className="select-container club">
+                    {itemsToRender.map((school, i) => (
+                            <SelectItem item={school} />
+                        ))}
+                        <button id="create_btn" className="black-btn" >Create</button>
                     </div>
-                    <div className="setup-img">
-                            <img src={setupImg} alt="HI" />
-                    </div>
-                </div> 
+                </div>
+                <div className="setup-img">
+                    <img src={setupImg} alt="HI" />
+                </div>
             </div>
-  )
+        </div>
+    )
 }
 
 export default Setup
