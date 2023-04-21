5402b527e187b2e83a983f435f038c917ace2e97
SivaPrasad43 <39110552+SivaPrasad43@users.noreply.github.com>
2023-04-21 08:34:13 +0530

    create-school-data-bug-fixed

--- src/components/SetupBox/SchoolSetup.tsx
+++ src/components/SetupBox/SchoolSetup.tsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from "react"
 import Select from "react-select"
 import "./Setup.scss"
-import setupImg from "../../assets/Kindergarten student-bro 1.png"
-import axios from "axios"
 import apiGateway from "../../service/apiGateway"
 
 interface SelectItemProps {
@@ -32,38 +30,6 @@ interface SchoolProps {
   title: string
 }
 
-const SelectItem: React.FC<SelectItemProps> = ({ item, list, onData }) => {
-  const [selectedDistrict, setSelectedDistrict] = useState("")
-  const [renderList, setRenderList] = useState([])
-  const handleChange = (e: any) => {
-    setSelectedDistrict(e.id)
-    console.log(e.id)
-    const handleData: any = {
-      id: e.id,
-      name: e.name,
-    }
-    console.log(handleData)
-    onData(handleData)
-  }
-
-  return (
-    <div className="setup-item" id="district">
-      <p>{item}</p>
-      <Select
-        options={list}
-        isSearchable={true}
-        isClearable={true}
-        placeholder={`Select a ${item}`}
-        getOptionValue={(option: any) => option.id}
-        getOptionLabel={(option: any) =>
-          item === "School" ? option.title : option.name
-        }
-        onChange={handleChange}
-      />
-    </div>
-  )
-}
-
 const SchoolSetup = () => {
   const [districts, setDistricts] = useState<DistrictProps[]>([])
   const [legislativeAssemblies, setLegislativeAssemblies] = useState<LegislativeAssemblyProps[]>([])
@@ -76,67 +42,6 @@ const SchoolSetup = () => {
   const [schoolSelectedId, setSchoolSelectedId] = useState("")
   const [schoolSelectedName, setSchoolSelectedName] = useState("")
 
-  useEffect(() => {
-    const fetchData = async () => {
-        apiGateway.get(`/api/v1/yip/district/`)
-        .then(({ data }) => {
-          const { districts } = data.response;
-          console.log("districts-axios :", districts);
-          setDistricts(districts);
-        })
-        .catch(error => console.error(error));
-      }
-    fetchData()
-  }, [])
-
-  useEffect(() => {
-    console.log("dist selected:", districtSelected)
-    if (districtSelected) {
-    const fetchData = async () => {
-      apiGateway.get(`/api/v1/yip/get-legislative-assembly/${districtSelected}/`)
-      .then(({ data }) => {
-        const { legislativeAssembly } = data.response;
-        console.log("leg-axios :", legislativeAssembly);
-        setLegislativeAssemblies(legislativeAssembly)
-      })
-      .catch(error => console.error(error));
-    }
-  fetchData()
-  }
-  }, [districtSelected])
-
-  useEffect(() => {
-    if (districtSelected) {
-      const fetchData = async () => {
-        apiGateway.get(`/api/v1/yip/get-blocks/${districtSelected}/`)
-        .then(({ data }) => {
-          const { block } = data.response;
-          console.log("block-axios :", block);
-          setBlocks(block)
-        })
-        .catch(error => console.error(error));
-      }
-    fetchData()
-    }
-  }, [districtSelected])
-
-  useEffect(() => {
-    const reqData: any = {
-      district: districtName,
-    }
-    if (districtSelected) {
-      const fetchData = async () => {
-        apiGateway.post(`/api/v1/yip/list-model-schools/`,reqData)
-        .then(({ data }) => {
-          const { institutions } = data.response;
-          console.log("school-axios :", institutions);
-          setSchool(institutions)
-        })
-        .catch(error => console.error(error));
-      }
-    fetchData()
-    }
-  }, [districtSelected])
 
   const handleDistrict = (data: any) => {
     setDistrictSelected(data.id)
@@ -144,6 +49,77 @@ const SchoolSetup = () => {
     setDistrictName(data.name)
   }
 
+
+
+  // Fetch District Data
+  useEffect(() => {
+    const fetchData = async () => {
+      apiGateway.get(`/api/v1/yip/district/`)
+        .then(({ data }) => {
+          const { districts } = data.response;
+          console.log("districts-axios :", districts);
+          setDistricts(districts);
+        })
+        .catch(error => console.error(error));
+    }
+    fetchData()
+  }, [])
+
+  // Fetch Legislative Assembly Data
+  useEffect(() => {
+    console.log("dist selected:", districtSelected)
+    if (districtSelected) {
+      const fetchData = async () => {
+        apiGateway.get(`/api/v1/yip/get-legislative-assembly/${districtSelected}/`)
+          .then(({ data }) => {
+            const { legislativeAssembly } = data.response;
+            console.log("leg-axios :", legislativeAssembly);
+            setLegislativeAssemblies(legislativeAssembly)
+          })
+          .catch(error => console.error(error));
+      }
+      fetchData()
+    }
+  }, [districtSelected])
+
+  // Fetch Block Data
+  useEffect(() => {
+    if (districtSelected) {
+      const fetchData = async () => {
+        apiGateway.get(`/api/v1/yip/get-blocks/${districtSelected}/`)
+          .then(({ data }) => {
+            const { block } = data.response;
+            console.log("block-axios :", block);
+            setBlocks(block)
+          })
+          .catch(error => console.error(error));
+      }
+      fetchData()
+    }
+  }, [districtSelected])
+
+  // Fetch Institute Data
+  useEffect(() => {
+    const reqData: any = {
+      district: districtName,
+    }
+    if (districtSelected) {
+      const fetchData = async () => {
+        apiGateway.post(`/api/v1/yip/list-model-schools/`, reqData)
+          .then(({ data }) => {
+            const { institutions } = data.response;
+            console.log("school-axios :", institutions);
+            setSchool(institutions)
+          })
+          .catch(error => console.error(error));
+      }
+      fetchData()
+    }
+  }, [districtSelected])
+
+
+
+  // Function for sending Data
   const sendData = (): any => {
     const postData: any = {
       club_name: schoolSelectedName,
@@ -153,32 +129,20 @@ const SchoolSetup = () => {
       district_id: districtSelected,
       block_id: blockSelectedId,
     }
-    const postOptions = {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(postData),
-    }
-
     const createData = async () => {
-      try {
-        const response = await fetch(
-          import.meta.env.VITE_BACKEND_URL + `/api/v1/yip/create-club/`,
-          postOptions
-        )
-        console.log(response)
-        const data = await response.json()
-        window.location.reload()
-        console.log("response : ", data)
-      } catch (error) {
-        console.error(error)
-      }
+      apiGateway.post(`/api/v1/yip/create-club/`, postData)
+        .then((response) => {
+          console.log("axios-response :", response);
+          window.location.reload()
+        })
+        .catch(error => console.error(error));
     }
     createData()
     console.log("data send!!")
   }
+
+
+
   return (
     <div className="white-container">
       <h3>Setup a new School</h3>
