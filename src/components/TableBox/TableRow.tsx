3fc0563cb4e05e53562bb1a8929420a96f4f6fbf
SivaPrasad43 <39110552+SivaPrasad43@users.noreply.github.com>
2023-04-21 08:10:29 +0530

    axios-schoolsetup

--- src/components/TableBox/TableRow.tsx
+++ src/components/TableBox/TableRow.tsx
@@ -1,26 +1,7 @@
 import React from "react";
+import Select from "react-select/dist/declarations/src/Select";
 
 let statusOptions = ["Orientation Scheduled","Connection established","Identified"]
-
-export const SchoolTableData = (data: any, key: number) => {
-  return (
-    <ul id="clubs_listed">
-      <li id="sl_no" className="value">1</li>
-      <li id="club_id" className="value">{data.data.name}</li>
-      <li id="district" className="value">{data.data.district}</li>
-      <li className="value">{data.data.legislative_assembly}</li>
-      <li className="value">{data.data.block}</li>
-      <li className="value editable"> 
-        <a className="table-btn completed" href="#">
-          {data.data.club_status}
-        </a>
-        <a id="edit">
-          <i className="fa-solid fa-pen-to-square"></i>Edit
-        </a>
-      </li>
-    </ul>
-  );
-};
 
 export const ClubTableData = (data:any,key:number) => {
     return (
