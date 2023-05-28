9186cc4441e5c797255096e66b40a2c17a507453
Bijo Mathew Jose <bijomathewjose@gmail.com>
2023-05-28 12:15:26 +0530

    updates

--- src/modules/Dashboard/modules/pages/Campus/layout/CampusLayout.tsx
+++ src/modules/Dashboard/modules/pages/Campus/layout/CampusLayout.tsx
@@ -40,22 +40,28 @@ const CampusLayout = () => {
     return (
         <div className='dash-container'>
             <div className='white-container'>
+
                 <div className='campus-sub-container-1'>
                     <div className='update-status-button' onClick={() => setDeleteCampus(true)}>Delete Campus </div>
 
                     <div className='update-status-button' onClick={() => setUpdateCampus(true)}>Update Status</div>
                 </div>
+
+
                 {deleteCampus && <DeleteModal id={''} cancel={() => setDeleteCampus(false)} />}
                 {updateCampus && <CampusModal campuStatus={campus?.status} deleteId={''} cancel={() => setUpdateCampus(false)} />}
 
                 <div className='campus-sub-container-2'>
                     <TitleNameTag title={'Campus'} name={campus?.campus} />
                     <TitleNameTag title={'Category'} name={campus?.category} />
+
                     <TitleNameTag title={'Zone'} name={campus?.zone} />
                     <TitleNameTag title={'District'} name={campus?.district} />
                     <TitleNameTag title={'Legislative Assembly'} name={campus?.legislativeAssembly} />
                     <TitleNameTag title={'Block '} name={campus?.block} />
+
                 </div>
+
                 <Identified date={campus?.identified} />
                 {campus?.confirmed && <Confirmed date={campus?.confirmed} />}
                 {campus?.connection && <Connection date={campus?.connection?.date} facilitator={campus?.connection?.facilitator} />}
@@ -65,8 +71,8 @@ const CampusLayout = () => {
                 <Connection date={campus?.connection?.date} facilitator={campus?.connection?.facilitator} />
                 <Orientation orientation={campus?.orientation} />
                 <Execom date={campus?.execom?.date} members={campus?.execom?.members} />
-            </div>
-        </div>
+            </div >
+        </div >
     )
 }
 
