ff392a83adb9c239736c4dd1e17de79d3363d96d
Jenin <jenin8282@gmail.com>
2023-06-05 06:53:39 +0530

    feat(bulk-upload):bulk-upload-added-with-toastify

--- src/modules/Dashboard/modules/pages/Intern/InternApi.ts
+++ src/modules/Dashboard/modules/pages/Intern/InternApi.ts
@@ -1,3 +1,15 @@
-import { Dispatch, SetStateAction } from "react"
-import { privateGateway } from "../../../../../services/apiGateway"
+import { Dispatch, SetStateAction } from "react";
+import { privateGateway } from "../../../../../services/apiGateway";
+import { tableRoutes } from "../../../../../services/urls";
+import { AxiosRequestConfig } from "axios";
 
+export const uploadSubmissions = (config: AxiosRequestConfig) => {
+    privateGateway
+        .post(tableRoutes.user.uploadSubmissions, config)
+        .then((response) => {
+            console.log(response);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+};
